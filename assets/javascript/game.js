d5a74e93f22bc4d2c5430e6749e9cd26f24db27e
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2018-09-25 23:03:32 -0500

    EOD changes
    changes to jquery, html, and readme

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+var winCount = 0;
+var lossCount = 0;
+var currentScore = 0;
+var targetNumber = 0;
+
+
+for (var i = 0; i < 4; i++) {
+
+    var imageCrystal = $("<img>");
+
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.addClass("crystal"+i);
+
+    imageCrystal.attr("src", "assets/images/crystal"+i+".png");
+
+    imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1) + 1));
+    $("#crystals").append(imageCrystal);
+}  
+
+
+function gameSetup() {
+    targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
+    $("#number-to-guess").text(targetNumber);
+
+    currentScore = 0;
+    $("#current-score").text(currentScore);
+
+
+    for (var i = 0; i < 4; i++) {
+        var imageCrystal = $(".crystal"+i);
+        imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1) + 1));
+    }
+}
+
+gameSetup();
+$(".crystal-image").on("click", function () {
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+
+    currentScore += crystalValue;
+
+    $("#currentScore").text(currentScore);
+
+    if (currentScore === targetNumber) {
+        alert("You win!");
+        winCount++;
+        $("#totalWins").text(winCount);
+        gameSetup();
+    } else if (currentScore >= targetNumber) {
+        alert("You lose!!");
+        lossCount++;
+        $("#totalLosses").text(lossCount);
+        gameSetup();
+    }
+
+});
